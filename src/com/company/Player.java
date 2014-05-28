fc510fe17c82440635752db7698bc7da8ba2731a
evagabriela <gabriela.osu@gmail.com>
2014-05-28 16:00:13 -0700

    Add team, number and age state in player

--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -3,15 +3,23 @@ package com.company;
 /**
  * Created by gzamudio on 5/27/14.
  */
-public class Player {
-    private final String name;
 
-    public Player(String name) {
+public class Player {
+    private String age;
+    private String number;
+    private String name;
+    private String team;
+
+    public Player(String name, String team, String number, String age) {
         this.name = name;
+        this.team = team;
+        this.number = number;
+        this.age = age;
     }
 
     @Override
     public String toString(){
-        return name;
+        return "Name:" + name + " Team:" + team + " Number:" + number + " Age:" + age;
+
     }
 }
