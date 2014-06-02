1b521a58ec3b8e04c87c1c615fe24c2b89716623
evagabriela <gabriela.osu@gmail.com>
2014-06-02 10:47:54 -0700

    Add menu option and command list for each option

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,17 +1,32 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import static java.util.Arrays.asList;
 
 public class Main {
 
-    public static void main(String[] args) {
-        PrintStream out = System.out;
-        List<Player> players = teamPlayers();
 
-        new Team(out, players).listPlayers();
+    public static void main(String[] args) throws IOException {
+        PrintStream printStream = System.out;
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        List<Player> players = teamPlayers();
+        Team team = new Team(printStream, players);
+
+        List<Command> commands = new ArrayList<Command>();
+        commands.add(new ListPlayersCommand(team));
+        commands.add(new FindPlayerCommand(printStream, bufferedReader, team));
+
+        Menu menu = new Menu(printStream, bufferedReader, commands);
+        Command command = menu.listMenuOptions();
+        command.execute();
+
     }
 
     private static List<Player> teamPlayers(){
