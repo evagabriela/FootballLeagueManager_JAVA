4caf449dc3c9ddf4a55ecb4e1a996bbca1a8c502
evagabriela <gabriela.osu@gmail.com>
2014-06-03 15:05:31 -0700

    Add for story to see the list of players and coaches on the team

--- test/com/company/CoachesTest.java
+++ test/com/company/CoachesTest.java
@@ -1,9 +1,19 @@
 package com.company;
 
+import org.junit.Test;
+
 /*
 I want the list of all players in the league to also list all of the coaches in the league.
  Coaches are not be findable like players are. Coaches have a name, team, and title.
  */
 public class CoachesTest {
+
+    @Test
+    public void shouldListCoachesWhenListPlayersFromTheSameTeam(){
+        Coaches coaches = new Coaches("Eva", "Ohio", "Head Coach");
+    }
+
+
+
 
 }
