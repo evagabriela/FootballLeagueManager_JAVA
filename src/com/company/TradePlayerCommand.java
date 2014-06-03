2c3940b70d216e9d6490ad933db26340ef1a1dea
evagabriela <gabriela.osu@gmail.com>
2014-06-03 16:39:01 -0700

    Add promptInput and readLine methods to get user input and update code in execute method to display request to user to enter information

--- src/com/company/TradePlayerCommand.java
+++ src/com/company/TradePlayerCommand.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public class TradePlayerCommand implements Command {
@@ -17,7 +18,28 @@ public class TradePlayerCommand implements Command {
 
     @Override
     public void execute() {
+        String playerName = promptedInput("Enter the name of the player: ");
+        String playerNumber = promptedInput("Enter the number of the player: ");
+        String newTeamInfo = promptedInput("Enter the name of the player's new team: ");
+        team.tradePlayer(playerName, playerNumber, newTeamInfo);
+//        here I can do display player ex. team.displayPlayer(playerName, playerNumber)
 
+
+    }
+
+    private String promptedInput(String prompt){
+        printStream.println(prompt);
+        return readLine();
+    }
+
+    private String readLine(){
+        String playerInfo;
+        try {
+            playerInfo = bufferedReader.readLine();
+        } catch (IOException e){
+            throw new InputErrorIOException();
+        }
+        return playerInfo;
     }
 
     @Override
