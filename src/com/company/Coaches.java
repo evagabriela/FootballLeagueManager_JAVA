94b0c5438002fbec257100002ea3621460872e38
evagabriela <gabriela.osu@gmail.com>
2014-06-03 11:47:40 -0700

    Add Coaches test class

---
+++ src/com/company/Coaches.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Coaches {
+    private String name;
+    private String team;
+    private String title;
+
+    public Coaches(String name, String team, String title){
+        this.name = name;
+        this.team = team;
+        this.title = title;
+    }
+
+
+}
---
+++ test/com/company/CoachesTest.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/*
+I want the list of all players in the league to also list all of the coaches in the league.
+ Coaches are not be findable like players are. Coaches have a name, team, and title.
+ */
+public class CoachesTest {
+
+}
