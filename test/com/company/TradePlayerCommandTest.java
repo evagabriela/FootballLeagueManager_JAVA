ce2aaf9e819208907347550a27bbddad38c5f34e
evagabriela <gabriela.osu@gmail.com>
2014-06-03 16:38:14 -0700

    Add test to check display message for option trade player and test prompts

--- test/com/company/TradePlayerCommandTest.java
+++ test/com/company/TradePlayerCommandTest.java
@@ -4,10 +4,13 @@ import org.junit.Before;
 import org.junit.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class TradePlayerCommandTest {
     private TradePlayerCommand command;
@@ -27,6 +30,19 @@ public class TradePlayerCommandTest {
     @Test
     public void shouldDisplayAMessageInMenuOption(){
         assertEquals("Trade Player", command.name());
+    }
+
+    @Test
+    public void shouldPromptToEnterNameOfPlayer(){
+        command.execute();
+        verify(printStream).println("Enter the name of the player: ");
+    }
+
+    @Test
+    public void shouldPromptToEnterNumberOfPlayer() throws IOException {
+        when(bufferReader.readLine()).thenReturn("Gaby");
+        command.execute();
+        verify(printStream).println("Enter the number of the player: ");
     }
 
 
