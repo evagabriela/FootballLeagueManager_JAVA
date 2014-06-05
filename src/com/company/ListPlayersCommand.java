1089fb64f26459f24a29499b7ca4d3a3b181208d
evagabriela <gabriela.osu@gmail.com>
2014-06-05 10:25:32 -0700

    Update code to include league instead of team

--- src/com/company/ListPlayersCommand.java
+++ src/com/company/ListPlayersCommand.java
@@ -1,22 +1,32 @@
 package com.company;
 
+import java.io.PrintStream;
+import java.util.HashSet;
+
 /**
  * Created by gzamudio on 5/29/14.
  */
 public class ListPlayersCommand implements Command {
+    private PrintStream printStream;
     private League league;
 
-    public ListPlayersCommand(League league) {
+    public ListPlayersCommand(League league, PrintStream printStream) {
         this.league = league;
+        this.printStream =printStream;
     }
 
     @Override
     public void execute() {
-        league.listPlayers();
+        HashSet<Player> players =  league.getListPlayers();
+
+        for (Player player: players ){
+            printStream.println(player);
+        }
+
     }
 
     @Override
     public String name() {
-        return "List all the players in a team";
+        return "List all the players in a league";
     }
 }
--- test/com/company/ListPlayersCommandTest.java
+++ test/com/company/ListPlayersCommandTest.java
@@ -3,6 +3,8 @@ package com.company;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
@@ -10,23 +12,25 @@ import static org.mockito.Mockito.verify;
 public class ListPlayersCommandTest {
     private League league;
     private ListPlayersCommand command;
+    private PrintStream printStream;
 
     @Before
     public void setUp(){
+        printStream = mock(PrintStream.class);
         league = mock(League.class);
-        command = new ListPlayersCommand(league);
+        command = new ListPlayersCommand(league, printStream);
 
     }
 
     @Test
     public void shouldDisplayMessageInAMenuOption(){
-        assertEquals("List all the players in a team", command.name());
+        assertEquals("List all the players in a league", command.name());
     }
 
     @Test
     public void shouldListAllPlayersWhenExecuteMethodIsCall(){
         command.execute();
-        verify(league).listPlayers();
+        verify(league).getListPlayers();
     }
 
 }
