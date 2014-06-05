a97da0e82e2e3e6c49c8c6e44a528a43e65b4a42
evagabriela <gabriela.osu@gmail.com>
2014-06-05 10:24:29 -0700

    Update player test to include league and remove team from it

--- test/com/company/PlayerTest.java
+++ test/com/company/PlayerTest.java
@@ -3,37 +3,36 @@ package com.company;
 import org.junit.Before;
 import org.junit.Test;
 
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 public class PlayerTest {
     private Player player;
-    private League league;
+    private Team team;
 
     @Before
     public void setUp(){
-        player = new Player("Gaby", "Ohio", "3", "26");
-        league = mock(League.class);
+
+        team = mock(Team.class);
+        player = new Player("Gaby", team, "3", "26");
     }
 
     @Test
     public void shouldDisplayFullPlayerInformationAsString(){
-        assertEquals("Name:Gaby Team:Ohio Number:3 Age:26", player.toString());
+
+        assertEquals("Name:Gaby Team:" + team+  " Number:3 Age:26", player.toString());
     }
 
-    @Test
-    public void shouldReturnTrueIfPlayerNameMatchesTheOneInTheTeam(){
-        when(league.findPlayer("Gaby", "3")).thenReturn(player);
-        assertTrue(player.hasName("Gaby"));
-    }
-
-    @Test
-    public void shouldReturnFalseIfPlayerNameDoesntMatchTheOneInTheTeam(){
-        when(league.findPlayer("Gaby", "3")).thenReturn(player);
-        assertFalse(player.hasName("Maria"));
-    }
+//    @Test
+//    public void shouldReturnTrueIfPlayerNameAndNumberMatchesTheOneInTheTeam(){
+//        when(team.findPlayer("Gaby", "3")).thenReturn(player);
+//        assertTrue(player.hasNameAndNumber("Gaby", "3"));
+//    }
+//
+//    @Test
+//    public void shouldReturnFalseIfPlayerNameDoesntMatchTheOneInTheTeam(){
+//        when(team.findPlayer("Gaby", "3")).thenReturn(player);
+//        assertFalse(player.hasNameAndNumber("Maria", "0"));
+//    }
 
 }
