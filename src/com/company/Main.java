ee046d75cb1bd9c7f4bdb547df6943dfdc54da9b
evagabriela <gabriela.osu@gmail.com>
2014-06-05 15:22:53 -0700

    Replace create teams by doing creation of individual teams, change command arguments to include league instead of team

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
-import java.util.*;
-
-import static java.util.Arrays.asList;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
@@ -14,36 +13,60 @@ public class Main {
         PrintStream printStream = System.out;
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         DoneState done = new DoneState(false);
+        League league = new League();
+        Team team1 = buildTeam1(printStream);
+        Team team2 = buildTeam2(printStream);
+        league.addTeam(team1);
+        league.addTeam(team2);
 
-        List<Player> players = teamPlayers();
-        Set<Coaches> coaches = teamCoaches();
-        League league = new League(printStream, players, coaches);
 
-        List<Command> commands = createCommands(league, printStream, bufferedReader, done);
+        List<Command> commands = createCommands(team1, league, printStream, bufferedReader, done);
         Menu menu = new Menu(printStream, bufferedReader, commands, done);
         FootballLeagueController controller = new FootballLeagueController(printStream, menu);
-        
+
         controller.start();
     }
 
-    private static Set<Coaches> teamCoaches() {
-        Set<Coaches> coaches = new HashSet<Coaches>();
-        coaches.add(new Coaches("Eva Atunga", "Ohio", "Head Coach"));
-        coaches.add(new Coaches("Miguel Zamudio", "Ohio", "Assistance Coach"));
-        return coaches;
+    private static Team buildTeam1(PrintStream printStream){
+        Team team1 = new Team(printStream,"team1");
+        Player player1 = new Player("Gaby", team1, "3", "26");
+        Player player2 = new Player("Maria", team1, "1", "30");
+        team1.addPlayer(player1);
+        team1.addPlayer(player2);
+
+        Coach coach1 = new Coach("Eva", team1, "Head Coach");
+        Coach coach2 = new Coach("Miguel", team1, "Assistance Coach");
+        team1.addCoach(coach1);
+        team1.addCoach(coach2);
+
+        return team1;
     }
 
-    private static List<Command> createCommands(League league, PrintStream printStream, BufferedReader bufferedReader, DoneState done){
+    private static Team buildTeam2(PrintStream printStream){
+        Team team2 = new Team(printStream,"team2");
+        Player player1 = new Player("Keith", team2, "2", "27");
+        Player player2 = new Player("Mario", team2, "4", "38");
+        team2.addPlayer(player1);
+        team2.addPlayer(player2);
+
+        Coach coach1 = new Coach("Wayne", team2, "Head Coach");
+        Coach coach2 = new Coach("Liz", team2, "Assistance Coach");
+        team2.addCoach(coach1);
+        team2.addCoach(coach2);
+
+        return team2;
+    }
+
+
+
+    private static List<Command> createCommands(Team team, League league, PrintStream printStream, BufferedReader bufferedReader, DoneState done){
         List<Command> commands = new ArrayList<Command>();
-        commands.add(new ListPlayersCommand(league));
+        commands.add(new ListPlayersCommand(league,printStream));
         commands.add(new FindPlayerCommand(printStream, bufferedReader, league));
-        commands.add(new ListTeamMembersCommand(league));
+        commands.add(new ListTeamMembersCommand(league,printStream,bufferedReader));
         commands.add(new TradePlayerCommand(printStream, bufferedReader, league));
         return commands;
     }
 
-    private static List<Player> teamPlayers(){
-        List<Player> players = asList(new Player("Gaby", "Ohio", "3", "26"), new Player("Maria", "Chicago", "5", "28"));
-        return players;
-    }
+
 }
