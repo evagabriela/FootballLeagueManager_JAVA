fc8b887edc69134c72254a6e7736abf27b8065f2
evagabriela <gabriela.osu@gmail.com>
2014-06-05 16:03:12 -0700

    Add ListPlayersByAgeCommand to list players older than the age specified by the user

---
+++ src/com/company/ListPlayersByAgeCommand.java
@@ -0,0 +1,64 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Set;
+
+public class ListPlayersByAgeCommand implements Command {
+    private final PrintStream printStream;
+    private final BufferedReader bufferedReader;
+    private final League league;
+
+    public ListPlayersByAgeCommand(PrintStream printStream, BufferedReader bufferedReader, League league) {
+
+        this.printStream = printStream;
+        this.bufferedReader = bufferedReader;
+        this.league = league;
+    }
+
+    @Override
+    public void execute() {
+        String playerAge = promptedInput("Older than what age, would you like me to list?");
+
+        try {
+            listPlayers(playerAge);
+
+        } catch (InputErrorIOException e){
+            printStream.println("Players not found");
+        }
+
+    }
+
+    private String promptedInput(String prompt) {
+        printStream.println(prompt);
+        return readLine();
+    }
+
+    private String readLine() {
+        String playerInfo;
+        try{
+            playerInfo = bufferedReader.readLine();
+        }catch (IOException e){
+            throw new InputErrorIOException();
+        }
+        return playerInfo;
+    }
+
+    public void listPlayers(String age){
+        printStream.println("The players are:" );
+
+        Set<Player> players = league.getListPlayersOlderThan(age);
+
+        for (Player player : players){
+
+            printStream.println(player.toString());
+        }
+    }
+
+
+    @Override
+    public String name() {
+        return "List all the players older than...";
+    }
+}
---
+++ test/com/company/ListPlayersByAgeCommandTest.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+import java.io.BufferedReader;
+import java.io.PrintStream;
+import static org.mockito.Mockito.mock;
+import static org.junit.Assert.assertEquals;
+
+public class ListPlayersByAgeCommandTest {
+
+    private ListPlayersByAgeCommand command;
+    private PrintStream printStream;
+    private BufferedReader bufferedReader;
+    private League league;
+
+
+    @Before
+    public void setUp(){
+        printStream = mock(PrintStream.class);
+        bufferedReader = mock(BufferedReader.class);
+        league = mock(League.class);
+        command = new ListPlayersByAgeCommand(printStream,bufferedReader, league);
+    }
+
+    @Test
+    public void shouldDisplayMessageAsOptionInMenu(){
+        assertEquals("List all the players older than...", command.name());
+    }
+
+    @Test
+    public void shouldGetListPlayersOlderThanTheSpecificAge() throws Exception {
+
+    }
+}
